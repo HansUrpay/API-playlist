13a2ff9bc0790dd9b36c89502a20c5afe7285e26
HansUrpay <hansurpayh@gmail.com>
2023-01-20 22:28:39 -0500

    add user_controller and user_routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { type Application } from "express";
-
+import router from "./routes/index.routes"
 const app: Application = express();
 
-app.use(express.json());
 
+import routes from "./routes/index.routes"
+
+app.use(express.json());
+app.use("/", router)
+
+
+// prueba
+app.use("/", routes)
 
 export default app;
-
---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,7 @@
+export * as users from "./users/user.controller"
+
+
+
+
+
+// export  { default as user_router } from "./users/network";
