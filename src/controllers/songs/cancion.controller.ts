7a89080d4fed31d0a552c06d382795e69f5a9cb6
brian24C <brianjosuecastro@hotmail.com>
2023-01-21 13:38:46 -0500

    acomodando codigo del controlador songs

--- src/controllers/songs/cancion.controller.ts
+++ src/controllers/songs/cancion.controller.ts
@@ -54,23 +54,21 @@ export const store = async (req: Request, res: Response): Promise<void> => {
       res.status(500).json({ ok: false, message: error });
     }
   };
+  
+export const modify = async (req: Request, res: Response): Promise<void> => {
+  try {
+  const id: number = parseInt(req.params.id);
+  const data = req.body;
 
+  await prisma.song.update({
+      where: { id },
+      data: data,
+  });
 
-
-  export const modify = async (req: Request, res: Response): Promise<void> => {
-    try {
-    const id: number = parseInt(req.params.id);
-    const data = req.body;
-
-    await prisma.song.update({
-        where: { id },
-        data: data,
-    });
-
-    res.status(201).json({ ok: true, message: "Song actualizado correctamente" });
-    } catch (error) {
-    res.status(500).json({ ok: false, message: error });
-    }
+  res.status(201).json({ ok: true, message: "Song actualizado correctamente" });
+  } catch (error) {
+  res.status(500).json({ ok: false, message: error });
+  }
 };
 
 export const deletee = async (req: Request, res: Response): Promise<void> => {
