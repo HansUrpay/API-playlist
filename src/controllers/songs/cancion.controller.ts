cf103217ae03eaec2ec55934c5efaaa58f16d57c
brian24C <brianjosuecastro@hotmail.com>
2023-01-21 11:55:16 -0500

    add metodo post al controlador songs

--- src/controllers/songs/cancion.controller.ts
+++ src/controllers/songs/cancion.controller.ts
@@ -15,3 +15,26 @@ export const findAll = async (_req: Request, res: Response): Promise<void> => {
     res.status(500).json({ ok: false, message: error });
   }
 };
+
+
+export const store = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name, artist, album, year, genre, duration, publico } = req.body;
+  
+      await prisma.song.create({
+        data: {
+            name,
+            artist,
+            album,
+            year,
+            genre,
+            duration,
+            publico,
+        },
+      });
+  
+      res.status(201).json({ ok: true, message: "Song creado correctamente" });
+    } catch (error) {
+      res.status(500).json({ ok: false, message: error });
+    }
+  };
