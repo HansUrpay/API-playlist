90c2b1bb9a74f32f6f8106416c652197ed6fc3ef
yoroih <roycristhopherqp@gmail.com>
2023-01-24 08:43:58 -0500

    feat/funciones eliminar canciones en playlist y playlist agregadas

--- src/controllers/playlists/playlist.controller.ts
+++ src/controllers/playlists/playlist.controller.ts
@@ -53,6 +53,22 @@ export const createPlaylist = async (req: Request, res: Response) => {
     }
 };
 
+export const deletePlaylist = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = _req.params;
+        await prisma.songsOnPlaylist.deleteMany({ where: { id_playlist: Number(id) } });
+        await prisma.playlist.delete({ where: { id: Number(id) } });
+
+        res.status(200).json({
+            ok: true,
+            message: `Playlist with id ${id} has been deleted.`,
+        });
+
+    } catch (error) {
+        res.status(500).json({ ok: false, message: error });
+    }
+};
+
 export const seeSongs = async (_req: Request, res: Response): Promise<void> => {
     try {
         const songOnPlaylist = await prisma.songsOnPlaylist.findMany();
@@ -65,7 +81,6 @@ export const seeSongs = async (_req: Request, res: Response): Promise<void> => {
         res.status(500).json({ ok: false, message: error });
     }
 };
-
 
 export const addSongs = async (req: Request, res: Response) => {
     try {
@@ -85,5 +100,26 @@ export const addSongs = async (req: Request, res: Response) => {
             ok: false,
             error: error,
         });
+    }
+};
+
+export const deleteSongFromPlaylist = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const { id_song, id_playlist } = _req.params;
+        const deletedSongOnPlaylist = await prisma.songsOnPlaylist.deleteMany({ where: { id_song: Number(id_song), id_playlist: Number(id_playlist) } });
+
+        if (deletedSongOnPlaylist) {
+            res.status(200).json({
+                ok: true,
+                message: `Song with id ${id_song} has been deleted from playlist with id ${id_playlist}.`,
+            });
+        } else {
+            res.status(404).json({
+                ok: false,
+                message: `Song with id ${id_song} on playlist ${id_playlist} not found.`,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ ok: false, message: error });
     }
 };
--- src/routes/playlists/playlist.routes.ts
+++ src/routes/playlists/playlist.routes.ts
@@ -4,10 +4,13 @@ import { verifyToken } from "../../auth/auth";
 
 const playlist_router = Router();
 
-playlist_router.get("/playlist", playlists.findplaylist);
-playlist_router.post("/playlist",playlists.createPlaylist);
+playlist_router.get("/playlist",playlists.findplaylist);
+playlist_router.post("/playlist", verifyToken,playlists.createPlaylist);
+playlist_router.delete("/playlist/:id",playlists.deletePlaylist);
 playlist_router.get("/playlistadd",playlists.seeSongs);
-playlist_router.post("/playlistadd", playlists.addSongs);
+playlist_router.post("/playlistadd", verifyToken,playlists.addSongs);
+playlist_router.delete("/playlistadd/:id_playlist/song/:id_song", playlists.deleteSongFromPlaylist);
+
 
 
 export default playlist_router;
