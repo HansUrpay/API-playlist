806762f67814c105e61a48983f8d6037f601526e
brian24C <brianjosuecastro@hotmail.com>
2023-01-24 10:52:26 -0500

    agregando verifytoken al metodo delete

--- src/routes/songs/cancion.routes.ts
+++ src/routes/songs/cancion.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { songs } from "../../controllers/index.controller";
-
+import { verifyToken } from "../../auth/auth";
 
 const song_router = Router();
 
@@ -8,6 +8,6 @@ song_router.get("/songs", songs.findAll);
 song_router.get("/songs/:id", songs.findOne);
 song_router.put("/songs/:id", songs.modify);
 song_router.post("/songs", songs.store);
-song_router.delete("/songs/:id", songs.deletee);
+song_router.delete("/songs/:id", verifyToken, songs.deletee);
 
 export default song_router;
