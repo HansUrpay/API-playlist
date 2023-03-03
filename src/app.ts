89c5fcc79dedb3206bdd1812db56d1f267b5e945
HansUrpay <hansurpayh@gmail.com>
2023-03-02 20:48:20 -0500

    update route

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 app.use(express.json());
 app.use("/api/v1", user_router)
 app.use("/api/v1", song_router)
-app. use("/api/v1", playlist_router)
+app.use("/api/v1", playlist_router)
 
 
 export default app;
